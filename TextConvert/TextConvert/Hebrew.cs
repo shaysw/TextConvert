5452d2bb6ab5a8db962ac2618e727612336c33b7
Shaysw <shaysw@gmail.com>
2021-05-29 09:46:54 +0300

    now with code

---
+++ TextConvert/TextConvert/Hebrew.cs
@@ -0,0 +1,57 @@
+﻿namespace TextConvert
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Hebrew
+    {
+        private static readonly Dictionary<char, char> EnglishCharsToHebrewChars = new Dictionary<char, char>
+        {
+            { 'q', '/' },
+            { 'w', '\'' },
+            { 'e', 'ק' },
+            { 'r', 'ר' },
+            { 't', 'א' },
+            { 'y', 'ט' },
+            { 'u', 'ו' },
+            { 'i', 'ן' },
+            { 'o', 'ם' },
+            { 'p', 'פ' },
+            { '[', ']' },
+            { ']', '[' },
+            { '\\', '\\'},
+            { 'a', 'ש' },
+            { 's', 'ד' },
+            { 'd', 'ג' },
+            { 'f', 'כ' },
+            { 'g', 'ע' },
+            { 'h', 'י' },
+            { 'j', 'ח' },
+            { 'k', 'ל' },
+            { 'l', 'ך' },
+            { ';', 'ף' },
+            { '\'', ',' },
+            { 'z', 'ז'},
+            { 'x', 'ס' },
+            { 'c', 'ב' },
+            { 'v', 'ה' },
+            { 'b', 'נ' },
+            { 'n', 'מ' },
+            { 'm', 'צ' },
+            { ',', 'ת' },
+            { '.', 'ץ' },
+            { '/', '.' },
+
+            // TODO: what about shifts?
+        };
+
+        public static string ConvertEnglishToHebrew(string englishString)
+        {
+            var hebrewStringBuilder = new StringBuilder();
+            englishString.ToList().ForEach(englishChar => hebrewStringBuilder.Append(EnglishCharsToHebrewChars.TryGetValue(char.ToLower(englishChar), out var hebrewChar) ? hebrewChar : englishChar));
+        
+            return hebrewStringBuilder.ToString();
+        }
+    }
+}
---
+++ TextConvert/TextConvertTests/HebrewTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TextConvertTests
+{
+    [TestClass]
+    public class HebrewTests
+    {
+        [TestMethod]
+        public void TestSingleCharLowerCaseConvert()
+        {
+            var englishInput = "a";
+            var hebrewOutput = "ש";
+
+            Assert.AreEqual(TextConvert.Hebrew.ConvertEnglishToHebrew(englishInput), hebrewOutput);
+        }
+
+        [TestMethod]
+        public void TestSingleCharUpperCaseConvert()
+        {
+            var englishInput = "A";
+            var hebrewOutput = "ש";
+
+            Assert.AreEqual(TextConvert.Hebrew.ConvertEnglishToHebrew(englishInput), hebrewOutput);
+        }
+
+        [TestMethod]
+        public void TestMultipleCharactersAllExistingConvert()
+        {
+            var englishInput = "ABCDEFG";
+            var hebrewOutput = "שנבגקכע";
+
+            Assert.AreEqual(TextConvert.Hebrew.ConvertEnglishToHebrew(englishInput), hebrewOutput);
+        }
+
+        [TestMethod]
+        public void TestMultipleCharactersNotAllExistingConvert()
+        {
+            var englishInput = "ABCDEFG asd 1223$%^^$";
+            var hebrewOutput = "שנבגקכע שדג 1223$%^^$";
+
+            Assert.AreEqual(TextConvert.Hebrew.ConvertEnglishToHebrew(englishInput), hebrewOutput);
+        }
+    }
+}
