2d5fba769dd0fee5942c09deb2add514da8814be
Shaysw <shaysw@gmail.com>
2021-05-29 15:20:49 +0300

    added asp.net core web app

---
+++ TextConvert/TextConvertWebApp/Controllers/ConvertEnglishToHebrewController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+namespace TextConvertWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConvertEnglishToHebrewController : ControllerBase
+    {
+        private readonly ILogger<ConvertEnglishToHebrewController> _logger;
+
+        public ConvertEnglishToHebrewController(ILogger<ConvertEnglishToHebrewController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{englishInputString}")]
+        public string Get(string englishInputString)
+        {
+            return TextConvert.Hebrew.ConvertEnglishToHebrew(englishInputString);
+        }
+    }
+}
