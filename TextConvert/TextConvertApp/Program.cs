9671fb8ecb16930b72a380a422e8057d49bb7c6a
Shaysw <shaysw@gmail.com>
2021-05-29 15:06:56 +0300

    commit after changing to .net 5

---
+++ TextConvert/TextConvertApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TextConvertApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputString = args[0];
+            Console.WriteLine(TextConvert.Hebrew.ConvertEnglishToHebrew(inputString)); 
+        }
+    }
+}
