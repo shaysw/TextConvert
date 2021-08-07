fea96ee320087e056c1539b3be72dac83878ef70
Shaysw <shaysw@gmail.com>
2021-08-07 17:31:52 +0300

    small fixes

--- TextConvert/TextConvert/KeyboardLayouts/Hebrew.cs
+++ TextConvert/TextConvert/KeyboardLayouts/Hebrew.cs
@@ -1,11 +1,8 @@
-﻿namespace TextConvert
-{
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Text;
+﻿using System;
+using System.IO;
 
+namespace TextConvert
+{
     public class Hebrew : EnglishQweryConvertibleKeyboardLayout
     {
         private const string _keyboardLayoutMappingsFileName = "hebrew.json";
