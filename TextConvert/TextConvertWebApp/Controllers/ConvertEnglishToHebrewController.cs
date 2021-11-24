e75c4f113e9adbc15b4c5c6413dacf6d4657b0fd
Jimmy Lee Jones <shaysw@gmail.com>
2021-11-24 19:42:38 +0200

    Add logging to EventServer

--- TextConvert/TextConvertWebApp/Controllers/ConvertEnglishToHebrewController.cs
+++ TextConvert/TextConvertWebApp/Controllers/ConvertEnglishToHebrewController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
+using Newtonsoft.Json;
+
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 using TextConvert;
@@ -16,18 +20,21 @@ namespace TextConvertWebApp.Controllers
     public class ConvertTextApiController : ControllerBase
     {
         private readonly ILogger<ConvertTextApiController> _logger;
+        private HttpClient _httpClient;
         private Dictionary<Language, EnglishQweryConvertibleKeyboardLayout> _convertibleKeyboardLayouts = new()
         {
             { Language.Hebrew, new Hebrew() }
         };
+        
 
         public ConvertTextApiController(ILogger<ConvertTextApiController> logger)
         {
             _logger = logger;
+            _httpClient = new HttpClient();
         }
 
         [HttpPost]
-        public async Task<ActionResult<string>> ConvertText(ConvertTextRequest convertTextRequest)
+        public async Task<ActionResult<string>> ConvertTextAsync(ConvertTextRequest convertTextRequest)
         {
             var sourceLanguage = convertTextRequest.SourceLanguage;
             var targetLanguage = convertTextRequest.TargetLanguage;
@@ -35,6 +42,7 @@ namespace TextConvertWebApp.Controllers
             var remoteIpAddress = Request.HttpContext.Connection.RemoteIpAddress.MapToIPv4();
             string convertedString;
 
+            // TODO: not sure this is the best approach, for example, eng to eng but hebrew input string (remains hebrew)
             if (sourceLanguage == targetLanguage)
             {
                 convertedString = stringToConvert;
@@ -61,8 +69,17 @@ namespace TextConvertWebApp.Controllers
                 }
             }
 
-            _logger.LogInformation($"[{remoteIpAddress}] : {stringToConvert}({sourceLanguage}) -> {convertedString}({targetLanguage})");
+            var message = $"[{remoteIpAddress}] : {stringToConvert}({sourceLanguage}) -> {convertedString}({targetLanguage})";
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, EventServerLogMessage.eventServerUrl);
+            request.Content = new StringContent(new EventServerLogMessage(message).ToString(),
+                                                Encoding.UTF8,
+                                                "application/json");
+
+            await _httpClient.SendAsync(request);
+            _logger.LogInformation(message);
             return convertedString;
         }
+
     }
 }
---
+++ TextConvert/TextConvertWebApp/Controllers/EventServerLogMessage.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace TextConvertWebApp.Controllers
+{
+    internal class EventServerLogMessage
+    {
+        public static string eventServerUrl = "http://127.0.0.1:8000/EventServer/audit_log/post_event";
+
+        [JsonProperty("sender")]
+        public const string sender = "TextConverter";
+
+        [JsonProperty("event_type")]
+        public const string eventType = "LOG";
+
+        [JsonProperty("application_id")]
+        public const int applicationId = 116;
+
+        [JsonProperty("value")]
+        public string value;
+
+
+        public EventServerLogMessage(string message)
+        {
+            this.value = message;
+        }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
