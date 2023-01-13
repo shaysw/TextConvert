35a40a69f2b955a1b2b4ac71ad8be47eaaf6b50c
Shay Schwartzburd <shaysw@gmail.com>
2023-01-14 01:38:06 +0200

    small fix

--- TextConvert/TextConvertWebApp/Controllers/ConvertEnglishToHebrewController.cs
+++ TextConvert/TextConvertWebApp/Controllers/ConvertEnglishToHebrewController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -22,7 +23,7 @@ namespace TextConvertWebApp.Controllers
         {
             { Language.Hebrew, new Hebrew() }
         };
-        
+
 
         public ConvertTextApiController(ILogger<ConvertTextApiController> logger)
         {
@@ -33,43 +34,44 @@ namespace TextConvertWebApp.Controllers
         [HttpPost]
         public async Task<ActionResult<string>> ConvertTextAsync(ConvertTextRequest convertTextRequest)
         {
-            var sourceLanguage = convertTextRequest.SourceLanguage;
-            var targetLanguage = convertTextRequest.TargetLanguage;
-            var stringToConvert = convertTextRequest.StringToConvert;
-            var remoteIpAddress = Request.HttpContext.Connection.RemoteIpAddress.MapToIPv4();
-            string convertedString;
-
-            // TODO: not sure this is the best approach, for example, eng to eng but hebrew input string (remains hebrew)
-            if (sourceLanguage == targetLanguage)
-            {
-                convertedString = stringToConvert;
-            }
-            else
-            {
-                // TODO: handle conversions between 2 languages which are not engllish
-                if (sourceLanguage == Language.English)
-                {
-                    if (!_convertibleKeyboardLayouts.ContainsKey(targetLanguage))
-                    {
-                        throw new Exception($"Mapping for {targetLanguage} not found");
-                    }
-                    convertedString = _convertibleKeyboardLayouts[targetLanguage].ConvertEnglishQwertyStringToConvertibleKeyboardLayout(stringToConvert);
-                }
-                else
-                // TODO: implicitly assuming target is english
-                {
-                    if (!_convertibleKeyboardLayouts.ContainsKey(sourceLanguage))
-                    {
-                        throw new Exception($"Mapping for {sourceLanguage} not found");
-                    }
-                    convertedString = _convertibleKeyboardLayouts[sourceLanguage].ConvertConvertibleKeyboardLayoutToEnglishQwertyString(stringToConvert);
-                }
-            }
-
-            var message = $"[{remoteIpAddress}] : {stringToConvert}({sourceLanguage}) -> {convertedString}({targetLanguage})";
-
             try
             {
+                var sourceLanguage = convertTextRequest.SourceLanguage;
+                var targetLanguage = convertTextRequest.TargetLanguage;
+                var stringToConvert = convertTextRequest.StringToConvert;
+                var remoteIpAddress = Request.HttpContext.Connection.RemoteIpAddress.MapToIPv4();
+                string convertedString;
+
+                // TODO: not sure this is the best approach, for example, eng to eng but hebrew input string (remains hebrew)
+                if (sourceLanguage == targetLanguage)
+                {
+                    convertedString = stringToConvert;
+                }
+                else
+                {
+                    // TODO: handle conversions between 2 languages which are not engllish
+                    if (sourceLanguage == Language.English)
+                    {
+                        if (!_convertibleKeyboardLayouts.ContainsKey(targetLanguage))
+                        {
+                            throw new Exception($"Mapping for {targetLanguage} not found");
+                        }
+                        convertedString = _convertibleKeyboardLayouts[targetLanguage].ConvertEnglishQwertyStringToConvertibleKeyboardLayout(stringToConvert);
+                    }
+                    else
+                    // TODO: implicitly assuming target is english
+                    {
+                        if (!_convertibleKeyboardLayouts.ContainsKey(sourceLanguage))
+                        {
+                            throw new Exception($"Mapping for {sourceLanguage} not found");
+                        }
+                        convertedString = _convertibleKeyboardLayouts[sourceLanguage].ConvertConvertibleKeyboardLayoutToEnglishQwertyString(stringToConvert);
+                    }
+                }
+
+                var message = $"[{remoteIpAddress}] : {stringToConvert}({sourceLanguage}) -> {convertedString}({targetLanguage})";
+
+
                 HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, EventServerLogMessage.eventServerUrl);
                 request.Content = new StringContent(new EventServerLogMessage(message).ToString(),
                                                     Encoding.UTF8,
@@ -82,13 +84,14 @@ namespace TextConvertWebApp.Controllers
                     });
 
                 _logger.LogInformation(message);
+                
+                return convertedString;
             }
             catch (Exception exception)
             {
-                Console.WriteLine("Could not perform logging to event server : {0}", exception.Message);
+                Console.WriteLine("Error converting string : {0}", exception.Message);
             }
-            
-            return convertedString;
+
         }
 
     }
